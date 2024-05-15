edb142f0094ea00b673865038b937e8e16003936
Akobir Abduganiev <akobir.abduganiev@ya.ru>
2024-05-15 13:58:51 +0500

    Initial commit

---
+++ src/main/java/uz/etc/etcfitness/EtcFitnessApplication.java
@@ -0,0 +1,13 @@
+package uz.etc.etcfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtcFitnessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtcFitnessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/etc/etcfitness/EtcFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.etc.etcfitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtcFitnessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
