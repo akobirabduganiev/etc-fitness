a9d9c8f092dd8568e5bac904fbe87d09909ef9e9
Akobir Abduganiev <akobir.abduganiev@ya.ru>
2024-05-15 14:11:26 +0500

    Add 'MessagingException' RuntimeException class
    Added a new 'MessagingException' class in the exceptions package which extends the RuntimeException class. This new exception is intended to handle any messaging related anomalies within the

---
+++ src/main/java/uz/etc/etcfitness/exception/MessagingException.java
@@ -0,0 +1,7 @@
+package uz.etc.etcfitness.exception;
+
+public class MessagingException extends RuntimeException {
+    public MessagingException(String message) {
+        super(message);
+    }
+}
